276cda5ffa6aaed30851a1b65ebcb39e0937166a
Ngdeveloper <naveeng.developer@gmail.com>
2020-08-05 17:49:38 +0530

    google search using jquery bootstrap base project

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Google to search only your site using jQuery & Bootstrap" />
+ 
+<title>
+Google to search only your site Demo - Javadomain
+</title>
+<!-- this can be finalizedd -->
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- header using bootstrap starts -->
+	<!-- Fixed navbar -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed"
+				data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+				aria-controls="navbar">
+				<span class="sr-only">GR Menu</span> <span class="icon-bar"></span>
+				<span class="icon-bar"></span> <span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand active" href="http://javadomain.in"><i class="fa fa-home"></i> &nbsp;Javadomain</a>
+			
+			<a class="navbar-brand active" href="http://javadomain.in/responsive-dynamic-header-using-jquery-php-bootstrap"> &nbsp;Tutorial</a>
+			
+			<a class="navbar-brand" target="_blank" href="https://www.couponzcorner.com">&nbsp;Best deals and Exclusive Offers for you</a>
+			
+			<a class="navbar-brand" target="_blank" href="https://1site.online">&nbsp;1site for all your needs</a>
+		</div>
+	</div>
+	</nav>
+	<!-- header using bootstrap ends -->
+	
+	<div class="container" style="margin-top: 50px;"> 
+	  <h3> Google to search only within javadomain.in</h3>
+	  <div id="no-more-tables">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Single post - 1 (www.javadomain.in) -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1630317480583873"
+     data-ad-slot="8555201327"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<br/>
+		<div class="col-lg-12 col-md-12 col-xs-12">		
+<h3 class="bg-primary center"> Enter any search term [java, php, jquery, css] (searches only in javadomain.in)</h3>
+<form action="http://www.google.co.in/search" method="get" target="_blank">
+<input type="text" class="form-control" name="as_q">
+<input type="hidden" name="sitesearch" class="form-control" value="javadomain.in">
+<span class="input-group-btn">
+</span>
+</form>
+</div>
+<br/>
+<br/>
+<br/>
+        </div>
+		
+		
+		
+            <table class="col-md-12 table-bordered table-striped table-condensed cf" style="margin-top:30px;">
+        		<thead class="cf">
+        			<tr>
+        				<th>Popular Articles</th>
+        			</tr>
+        		</thead>
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Single post - 1 (www.javadomain.in) -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1630317480583873"
+     data-ad-slot="8555201327"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+				
+        		<tbody>
+        			 
+					 
+					 <tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/25-tips-to-use-google-effectively/">How to use google very effectively tips?</a></div></td>
+        			</tr>
+					 
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://demo.javadomain.in/type_writing_script/">Improve your type writing speed with our type writing script</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://recharge.javadomain.in/ors/">Start your own Paytm/Freecharge site with paytm/freecharge clone script</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/how-to-create-a-mobile-recharge-website/">How to create online recharge website ?</a></div></td>
+        			</tr>
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/autocomplete-using-jquery-php-and-mysql/">Autocomplete using jQuery, PHP and mysql</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/freecharge-paytm-style-mobile-number-auto-operator-loading/">Freecharge/Paytm style mobile number auto operator loading</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/tips-clear-ocjp-6-scjp-certification/">Tips to Clear OCJP 6 (SCJP) certification</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/google-style-autosuggest-using-google-suggest-jquery-plugin/">Google style AutoSuggest using Google Suggest jQuery</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/responsive-bootstrap-3-masonry-image-gallery-example/">Responsive Bootstrap 3 Masonry Image Gallery Example</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/how-to-create-book-price-comparison-website/">How to create a book comparison website ?</a></div></td>
+        			</tr>
+        			
+        			<tr>
+        				<td ><div id="pop_articles"> <a href="http://www.javadomain.in/pnr-status-script-using-php-jquery-and-ajax/">PNR status script using jQuery, PHP and Ajax</a></div></td>
+        			</tr>
+        			
+        			 
+        		</tbody>
+        	</table>
+		
+	  
+ <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Single post - 1 (www.javadomain.in) -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1630317480583873"
+     data-ad-slot="8555201327"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+    </div> <!-- /container -->
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	</body>
+</html>
